d19bb5c846886767a4d325c78da5649ea81138de
Wei Qiu <wei@qiu.es>
2013-05-30 21:10:44 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.clojars.gjahad/debug-repl "0.3.1"
+(defproject org.clojars.gjahad/debug-repl "0.4.0-SNAPSHOT"
   :description "Clojure REPL that is aware of surrounding lexical scope"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
 
